9e7662ada84bed28175deb6d91a0f7d2ecf4a4ed
İsmail Namdar <namdari@itu.edu.tr>
2019-08-09 00:11:06 +0300

    refactor(routes): change Home route to batman icon

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -1,11 +1,12 @@
-import { lazy } from "react";
+import React, { lazy } from "react";
+import { GiBatMask } from "react-icons/gi";
 
 const Home = lazy(() => import('../pages/Home'));
 const ShowDetail = lazy(() => import('../pages/ShowDetail'));
 
 const routes = [
 	{
-		name: "Home",
+		name: <GiBatMask/>,
 		path: "/",
 		exact: true,
 		Component: Home
