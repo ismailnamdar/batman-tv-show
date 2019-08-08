c03ea5e5f39c0cbaf14638a97595a41e13c2470a
İsmail Namdar <namdari@itu.edu.tr>
2019-08-08 22:38:24 +0300

    feat(App): connect to redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useTranslation } from "react-i18next";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import AppRouter from "./ui/views/AppRouter";
-import {useTranslation} from "react-i18next";
 
 const App = () => {
 	const { t } = useTranslation("translations");
 	document.title = t("titles.batmanTvShows");
-	return <AppRouter/>;
+	return <Provider store={store}>
+		<AppRouter/>
+	</Provider>;
 };
 
 export default App;
