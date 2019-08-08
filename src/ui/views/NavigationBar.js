0000f7656f3c4ff26a0af82701a4f357bcd2b5c8
İsmail Namdar <namdari@itu.edu.tr>
2019-08-08 04:35:35 +0300

    fix(NavigationBar): make all block clickable

--- src/ui/views/NavigationBar.js
+++ src/ui/views/NavigationBar.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 import GlobeItem from "../components/GlobeItem";
 
 const NavigationBar = () => <ul className="nav">
-	{routes.map(({name, path, right}) => name != null && <li key={name + path} className={right ? "right" : "left" }><Link to={path}>{name}</Link></li>)}
+	{routes.map(({name, path, right}) => name != null && <Link to={path}><li key={name + path} className={right ? "right" : "left" }>{name}</li></Link>)}
 	<GlobeItem/>
 </ul>;
 
