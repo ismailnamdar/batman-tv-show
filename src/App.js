65cf27ca2dac30a0b7e652c7fb64c48d9ee493a9
İsmail Namdar <namdari@itu.edu.tr>
2019-08-08 04:34:03 +0300

    feat(document): title add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import AppRouter from "./ui/views/AppRouter";
+import {useTranslation} from "react-i18next";
 
-const App = () => <AppRouter/>;
+const App = () => {
+	const { t } = useTranslation("translations");
+	document.title = t("titles.batmanTvShows");
+	return <AppRouter/>;
+};
 
 export default App;
