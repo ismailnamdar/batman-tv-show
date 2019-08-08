66c1fb7e2bce15fc1b092a594b5138f0c0e7b2b9
İsmail Namdar <namdari@itu.edu.tr>
2019-08-08 22:36:38 +0300

    fix(NavigationBar): add key

--- src/ui/views/NavigationBar.js
+++ src/ui/views/NavigationBar.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 import GlobeItem from "../components/GlobeItem";
 
 const NavigationBar = () => <ul className="nav">
-	{routes.map(({name, path, right}) => name != null && <Link to={path}><li key={name + path} className={right ? "right" : "left" }>{name}</li></Link>)}
+	{routes.map(({name, path, right}) => name != null && <Link key={path} to={path}><li key={name + path} className={right ? "right" : "left" }>{name}</li></Link>)}
 	<GlobeItem/>
 </ul>;
 
