39bf331040aa8e78e87aa9ca0e9bb22164949fe4
İsmail Namdar <namdari@itu.edu.tr>
2019-08-09 12:12:15 +0300

    style(manifest): add icon, title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { useTranslation } from "react-i18next";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import AppRouter from "./ui/views/AppRouter";
 
 const App = () => {
-	const { t } = useTranslation("translations");
-	document.title = t("titles.batmanTvShows");
 	return <Provider store={store}>
 		<AppRouter/>
 	</Provider>;
